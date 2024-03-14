8eea360e327e4f2c57f7cba11795f9a818eee867
lmwei9 <646297821@qq.com>
2024-03-14 11:22:23 +0800

    添加py

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+from day09.tool.web_key import WebKey
+from selenium.webdriver.chrome.service import Service
+
+@pytest.fixture()
+def driver1():
+    driver12 = webdriver.Chrome()
+
+    yield driver12
+
+    driver12.quit()
